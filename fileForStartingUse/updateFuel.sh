a33fad8fecabb49503e9a95854d65a00fb540f1f
guia78 <>
2023-06-06 17:07:29 +0200

    Version 1.0.0 [Bot]Ti Open Data

--- fileForStartingUse/updateFuel.sh
+++ fileForStartingUse/updateFuel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Update DB for Fuel http://www.sviluppoeconomico.gov.it/index.php/it/open-data/elenco-dataset/2032336-carburanti-prezzi-praticati-e-anagrafica-degli-impianti
+# Update DB for Fuel https://www.mimit.gov.it/index.php/it/open-data/elenco-dataset/carburanti-prezzi-praticati-e-anagrafica-degli-impianti
 
 
 # define database connectivity
@@ -18,11 +18,11 @@ rm $_csv_directory_registry
 echo "\n \n"
 
 # download file csv
-echo "DOWNLOAD file CSV from site Sviluppo economico \n \n"
+echo "DOWNLOAD file CSV from site Ministero delle Imprese e del Made in Italy \n \n"
 sleep 5
 
-wget -O $_csv_directory_price --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0" "http://www.sviluppoeconomico.gov.it/images/exportCSV/prezzo_alle_8.csv"
-wget -O $_csv_directory_registry --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0" "http://www.sviluppoeconomico.gov.it/images/exportCSV/anagrafica_impianti_attivi.csv"
+wget -O $_csv_directory_price --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0" "https://www.mimit.gov.it/images/exportCSV/prezzo_alle_8.csv"
+wget -O $_csv_directory_registry --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0" "https://www.mimit.gov.it/images/exportCSV/anagrafica_impianti_attivi.csv"
 
 echo "\n \n"
 
@@ -32,7 +32,7 @@ mysqlimport --columns="idImpianto,descCarburante,prezzo,isSelf,@dtComu) SET dtCo
 sleep 5
 
 echo "\n END UPDATE PRICE"
-echo "Update DB for REGISTRY CSV from site Sviluppo economico \n \n"
+echo "Update DB for REGISTRY CSV from site Ministero delle Imprese e del Made in Italy \n \n"
 mysqlimport --ignore-lines=2 --fields-terminated-by=';' --lines-terminated-by="\n" -u $_db_user -p$_db_password $_db $_csv_directory_registry --delete
 echo "\n END UPDATE REGISTRY \n \n \n"
 
