1d5f0d10b04a6049f8b9031815aadb6d98a77a36
guia78 <>
2023-06-07 22:55:07 +0200

    Version 1.0.0 [Bot]Ti Open Data

--- config.php
+++ config.php
@@ -3,15 +3,15 @@
  * Variabili del Token [Bot]Ti
  * Define this variable immediatly. Insert the token of bot. Readme: https://core.telegram.org/bots#botfather
  */ 
-define('BOT_TOKEN', 'TOKEN TELEGRAM');    //Insert number/letter of id token   149655063:AAE4QGfuhxEXk2JNacYkkaqGQmQZYYQ9270
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('BOT_TOKEN', 'TOKEN TELEGRAM');    // Insert number/letter of id token example: 143355123:AAE4XCfuhxEXk2JNacYkkaqGQmQZYYQ9270
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/'); // Don't change
 
 /*
  * Variabili del database Mysql
  * Define this variable immediatly. This is variable of Mysql Server
  */ 
-$GLOBALS['mysql_host']='localhost'; //Ip server Mysql
-$GLOBALS['mysql_port']='3306';      //Port server Mysql
-$GLOBALS['mysql_user']='admin';     //User database
-$GLOBALS['mysql_pass']='PASSWORD';//Password database
-$GLOBALS['mysql_db']='telegram';    //Don't change
+$GLOBALS['mysql_host']='localhost'; // Ip server Mysql
+$GLOBALS['mysql_port']='3306';      // Port server Mysql or change
+$GLOBALS['mysql_user']='admin';     // User database
+$GLOBALS['mysql_pass']='PASSWORD';  // Password database
+$GLOBALS['mysql_db']='telegram';    // Don't change
--- fileForStartingUse/cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Script for start the Bot in the server
-#
-
-/usr/bin/php -f '/var/www/directory_install_platform/demone.php' > /dev/null 2>&1
-
