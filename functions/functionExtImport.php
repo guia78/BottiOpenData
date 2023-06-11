706a0b0ec85faa06676bd6f4dcfc4fbeadef8b82
guia78 <>
2023-06-11 17:13:38 +0200

    Version 1.0.0 [Bot]Ti Open Data

--- functions/functionExtImport.php
+++ functions/functionExtImport.php
@@ -1,12 +1,20 @@
 <?php
 require_once ('functionExt.php');
 require_once (dirname(__FILE__).'/../config.php');
+
 /* Delete all DB External */
 dbExtDeleteAll(); 
-/* Import Ical Events into DB */
-Ics('www.parks.it/ical/parks_generale.ics', 'Parchi Fvg', '1', '0');
 
-/* Import also information into DB */
+/* Import Ical Events into DB
+ * 
+ * This function is only for example
+ * 
+ * Don't use in this context
+*/
+Ics('www.parks.it/ical/parks_generale.ics', 'Parchi Fvg', '1', '0');
+ 
+
+/* Import also information into DB, forma JSON */
 WiFiFVG('https://www.insiel.it/dati/fvgwifi.json'); // New json may 2023
 farmOnlineFVGExt('https://sapi.sanita.fvg.it/farmacie/orario?APID=PM3EF4Q9CSNFJGW0LSSDVFBM81NLPGV17TSZ2XEAGY2VDQJNNF6T0HCUGLUOVZ3K');
 ?>
